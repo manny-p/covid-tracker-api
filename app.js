822ccc23cd0ac1a580310d3c41de7518775e6a3b
Manny <hi@mannyparra.com>
2020-08-25 20:36:58 -0600

    add set route for user to save a country, fix req bug

--- app.js
+++ app.js
@@ -3,24 +3,21 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const api = require('./routes/api/api');
 require('dotenv').config();
 require('./src/db');
-const auth = require('./middleware/auth');
 
-// new instance
+// a new instance of express
 const app = express();
 app.use(morgan('combined'));
 app.use(cors());
 app.use(helmet());
-app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.json({extended: true}))
 
 // middleware
-app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 // custom routes
-app.use('/api', auth, api)
+require('./routes/api/')(app)
 require('./routes/users/')(app)
 
 app.listen(process.env.PORT, () => {
