a3a9ea8edf16fa759d1676ee07a5dde82b222573
Manny <hi@mannyparra.com>
2020-08-31 00:44:14 -0600

    refactor save route

--- routes/api/save.js
+++ routes/api/save.js
@@ -5,17 +5,18 @@ const User = require("../../models/User")
 // @access   Private
 
 module.exports = async (req, res) => {
-console.log(req.body.countries)
+    console.log(req.body.countries)
     try {
         // Get a copy of the user database object
-        const results = await User.findById(req.user.user.id)
+        const user = await User.findById(req.user.user.id)
 
         // constructing a Set of countries
-        const countrySet = new Set(results.countries)
+        const countrySet = new Set(user.countries)
 
 
-        const parsedCountries = JSON.parse(req.body.countries)
-    
+        const parsedCountries = req.body.countries
+        console.log(parsedCountries);
+
         // console.log('fuckkkkkkyioudskdhcbshjhb' + req.body.countries)
         // debug complete
 
@@ -23,20 +24,24 @@ console.log(req.body.countries)
         parsedCountries.forEach(country => countrySet.add(country))
 
         // merge the countries the user wants to follow
-        results.countries = Array.from(countrySet)
+        user.countries = Array.from(countrySet)
 
-        console.log(`results ${results.countries}`)
+        console.log(`THIS BITCH IS USER.Countries!!!!!!!! ${user.countries}`)
 
         // Save the updated object into the database / save country results
-        await results.save()
+        await user.save()
 
         // remove the pw from user object
-        results.password = undefined
+        user.password = undefined
+    
 
         // return the object to the client via JSON response.
-        res.status(200).json({results})
+        console.log(user)
+
+        res.status(200).json({_id: user._id, countries: user.countries})
 
     } catch (err) {
+        console.log(err)
         res.status(500).json({msg: `Server Error ${err.message}`})
     }
 }
